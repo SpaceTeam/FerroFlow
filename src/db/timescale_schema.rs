f6cf2789ae5a7127dbb882d712c54f8f38851923
Michael Debertol <michael.debertol@gmail.com>
2026-03-13 02:44:32 +0100

    add comment about primary key to diesel table schema

--- src/db/timescale_schema.rs
+++ src/db/timescale_schema.rs
@@ -1,6 +1,8 @@
 // Manually generated schema for TimescaleDB hypertable. This is necessary because Diesel's CLI tool doesn't support generating schemas for tables that do not include a primary key.
 
 diesel::table! {
+    // (timestamp, node_id, field_id) is not the actual primary key (this table does not have any), but specified for Diesel to work with it.
+    // This is fine because we will never be doing updates or deletes on this table.
     field_logs (timestamp, node_id, field_id) {
         timestamp -> Timestamptz,
         node_id -> Nullable<Int2>,
