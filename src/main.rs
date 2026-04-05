d48dc6a111433d3d94cc8971acf66227c0d353e1
Michael Debertol <michael.debertol@gmail.com>
2026-04-05 03:31:23 +0200

    add heartbeat thread
    - send heartbeat requests at regular intervals to all nodes
    - register heartbeat responses

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,21 @@ fn main() -> Result<()> {
 
     let event_dispatcher = events::EventDispatcher::new();
 
+    let node_manager = nodes::NodeManager::new(&event_dispatcher);
     let _ = std::thread::scope::<'_, _, Result<()>>(|scope| {
-        let node_manager = nodes::NodeManager::new(&event_dispatcher);
         can::spawn_can_threads(&["vcan0"], &event_dispatcher, scope)?;
         db::spawn_logging_worker(
             "postgres://postgres:@localhost/ferroflow".into(),
             &event_dispatcher,
             scope,
         )?;
-        nodes::spawn_node_manager_thread(node_manager, &event_dispatcher, scope);
+        nodes::spawn_can_msg_handler_thread(&node_manager, &event_dispatcher, scope);
+        nodes::spawn_heartbeat_thread(
+            &node_manager,
+            std::time::Duration::from_secs(1),
+            &event_dispatcher,
+            scope,
+        );
 
         Ok(())
     });
--- src/nodes/mod.rs
+++ src/nodes/mod.rs
@@ -3,25 +3,65 @@
 mod can_node;
 mod node_manager;
 
-use std::sync::mpsc;
+use std::{
+    sync::mpsc::{self, RecvTimeoutError},
+    time::{Duration, Instant},
+};
 
 pub use node_manager::NodeManager;
 
 use crate::events;
 
-pub fn spawn_node_manager_thread<'a>(
-    node_manager: NodeManager<'a>,
+pub fn spawn_can_msg_handler_thread<'a>(
+    node_manager: &'a NodeManager<'a>,
     event_dispatcher: &'a events::EventDispatcher,
     scope: &'a std::thread::Scope<'a, '_>,
 ) {
     let (tx, rx) = mpsc::channel::<events::Event>();
-    event_dispatcher.subscribe(tx, "NodeManager thread");
+    event_dispatcher.subscribe(tx, "Can message handler thread");
     scope.spawn(move || {
         while let Ok(event) = rx.recv() {
             if let events::Event::CanMessageReceived { id, message } = event
                 && let Err(error) = node_manager.handle_can_message_from_node(id, message)
             {
                 eprintln!("Error handling CAN message in NodeManager: {error:#}");
+            }
+        }
+    });
+}
+
+pub fn spawn_heartbeat_thread<'a>(
+    node_manager: &'a NodeManager<'a>,
+    interval: Duration,
+    event_dispatcher: &'a events::EventDispatcher,
+    scope: &'a std::thread::Scope<'a, '_>,
+) {
+    let (tx, rx) = mpsc::channel::<events::Event>();
+    event_dispatcher.subscribe(tx, "Heartbeat thread");
+
+    scope.spawn(move || {
+        if let Err(error) = node_manager.dispatch_heartbeat_requests() {
+            eprintln!("Error dispatching heartbeat requests: {error:#}");
+        }
+        let mut next_heartbeat_at = Instant::now() + interval;
+
+        loop {
+            match rx.recv_timeout(next_heartbeat_at - Instant::now()) {
+                Ok(events::Event::Shutdown) => break,
+                Ok(_) => {}
+                Err(RecvTimeoutError::Timeout) => {
+                    if let Err(error) = node_manager.dispatch_heartbeat_requests() {
+                        eprintln!("Error dispatching heartbeat requests: {error:#}");
+                    }
+
+                    next_heartbeat_at += interval;
+
+                    // edge case: if next_heartbeat_at is already in the past, skip to now.
+                    if next_heartbeat_at < Instant::now() {
+                        next_heartbeat_at = Instant::now();
+                    }
+                }
+                Err(RecvTimeoutError::Disconnected) => break,
             }
         }
     });
